f2f6fdc4076aa3190e92bc27fe257a2586a8fd4a
wagnojunior <wagnojunior@gmail.com>
2022-12-18 12:24:21 +0900

    Added chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // homeHandler handles http requests to the home page
@@ -31,26 +33,16 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>FAQ Page</h1><p>Q: Question number one? <p>A: Answer number one.")
 }
 
-type Router struct{}
-
-// Router implements the Handler interface by defining the ServeHTTP method
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	// Creates a new chi router
+	r := chi.NewRouter()
+
+	// Routes
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 
 	// Starts the server
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
