5e458af41c4bb2d9fef4f30d4ff952b25a51df4e
wagnojunior <wagnojunior@gmail.com>
2022-12-18 18:52:53 +0900

    Added home template

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/wagnojunior/lenslocked
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.8 // indirect
+require github.com/go-chi/chi/v5 v5.0.8
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,13 +11,28 @@ import (
 
 // homeHandler handles http requests to the home page
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	bio := `<script>alert("HaHa, you have been h4x0r3d!");</script>`
-
 	// Sets the content type of the response header
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	// Writes a html tag to the response writer w
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1><p>Bio:"+bio+"</p>")
+	// Parses the template home.gohtml located in the folder <templates>
+	// If there is an error parsing, it will be handled here (invalid function in the template)
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		log.Printf("parsing templates: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+
+	// Executes the template tpl without any data (nil)
+	// If there is an error rendering, it will be handled here (invalid field in the template)
+	// This approach writes to the response writer until an error is detected (if any). If an error
+	// is detected half-way through the execution, then the webpage will be half rendered
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing templates: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 // contactHandler handles the http requests to the contact page
