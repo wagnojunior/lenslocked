5849fd7c8e3d555a6344b86a731b4804c05a74b6
wagnojunior <wagnojunior@gmail.com>
2022-12-18 16:24:34 +0900

    Chi exercise

--- main.go
+++ main.go
@@ -25,12 +25,25 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:wagnojunior@gmail.com\">wagnojunior@gmail.com</a>.")
 }
 
+// faqHandler handles the http request to the faq page
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	// Sets the content type of the response header
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	// Writes a html tag to the respponse writer
 	fmt.Fprintf(w, "<h1>FAQ Page</h1><p>Q: Question number one? <p>A: Answer number one.")
+}
+
+// userHandler handles the http request to the user page
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	// Sets the content type of the response header
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	// Fetch the URL parameter userID
+	userID := chi.URLParam(r, "userID")
+
+	// Writes a html tag to the respponse writer
+	fmt.Fprintf(w, "<h1> Welcome back, user %s</h1>", userID)
 }
 
 func main() {
@@ -41,6 +54,7 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/user/{userID}", userHandler)
 
 	// Starts the server
 	fmt.Println("Starting the server on :3000...")
