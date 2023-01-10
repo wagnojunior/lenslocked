05cbbcb112335776705a824a117a5dae766e1b4f
Wagno <wagnojunior@gmail.com>
2023-01-10 22:39:04 +0900

    Added a method to create users

--- models/users.go
+++ models/users.go
@@ -1,11 +1,17 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID           int
 	Email        string
-	PasswordHash int
+	PasswordHash string
 }
 
 // UserService is a type that has the database connection as a field.
@@ -17,4 +23,37 @@ type User struct {
 // 2) possibility to use interface (don't fully understand this one yet)
 type UserService struct {
 	DB *sql.DB
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	// Makes sure all emails are lower case
+	email = strings.ToLower(email)
+
+	// Generates a []byte hashed password and converts it to string
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	passwordHash := string(hashedBytes)
+
+	// Creates a new user object with the information that is being writted to the DB
+	user := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+
+	// Creates a new user using the `email` and the `passwordHash`
+	row := us.DB.QueryRow(`
+		INSERT INTO users (email, password_hash)
+		VALUES ($1, $2) RETURNING id`,
+		email, passwordHash)
+
+	// Scans for the id that was returned and saves if into `user.ID`
+	err = row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	return &user, nil
+
 }
