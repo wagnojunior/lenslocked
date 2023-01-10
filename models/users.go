3d3a65689cb7189d0cec66f927bfbac66134834a
Wagno <wagnojunior@gmail.com>
2023-01-10 22:00:58 +0900

    Added to model users.go

--- models/users.go
+++ models/users.go
@@ -1,7 +1,20 @@
 package models
 
+import "database/sql"
+
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash int
+}
+
+// UserService is a type that has the database connection as a field.
+// There are two main reasons why this approch is preferred:
+// 1) decouple the methods associated with this type (i.e.: Create method)
+// from the underlying database implementation. In order words, no prior
+// knowledge of how the database was implemented is necessary for the
+// associated methods to be called.
+// 2) possibility to use interface (don't fully understand this one yet)
+type UserService struct {
+	DB *sql.DB
 }
