8b20b1fee8fb1ca15c4f29a51741763fd984f7de
Wagno <wagnojunior@gmail.com>
2023-01-12 18:33:49 +0900

    Added comments

--- models/users.go
+++ models/users.go
@@ -8,23 +8,19 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// User defines the user model
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash string
 }
 
-// UserService is a type that has the database connection as a field.
-// There are two main reasons why this approch is preferred:
-// 1) decouple the methods associated with this type (i.e.: Create method)
-// from the underlying database implementation. In order words, no prior
-// knowledge of how the database was implemented is necessary for the
-// associated methods to be called.
-// 2) possibility to use interface (don't fully understand this one yet)
+// UserService defines the connection to the DB
 type UserService struct {
 	DB *sql.DB
 }
 
+// Create creates a new user
 func (us *UserService) Create(email, password string) (*User, error) {
 	// Makes sure all emails are lower case
 	email = strings.ToLower(email)
