ead7063f4e7bfcc64dc08b8d1d534ffda8a7b3b1
Wagno <wagnojunior@gmail.com>
2023-01-18 09:39:08 +0900

    Added rand package for generating session tokens

---
+++ rand/rand.go
@@ -0,0 +1,34 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+// Bytes returns a random byte slice of size `n` and an error using crypto/rand package
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("Bytes: %w", err)
+	}
+
+	return b, nil
+}
+
+// String returns returns a random string of size `n` and an error using the crypto/rand package
+func String(n int) (string, error) {
+	b, err := Bytes(n)
+	if err != nil {
+		return "", fmt.Errorf("String: %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+const SessionTokenBytes = 32
+
+// SessionToken returns a random string of size 32 and and error using the crypto/rand package
+func SessionToken() (string, error) {
+	return String(SessionTokenBytes)
+}
