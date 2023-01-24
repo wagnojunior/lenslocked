d8646231ff76da7374187ba620f4dc58c8348526
wagnojunior <wagnojunior@gmail.com>
2023-01-24 10:53:56 +0900

    Added ON DELETE CASCADE to sessions table

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -2,5 +2,5 @@ CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
     user_id INT UNIQUE,
     token_hash TEXT UNIQUE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) 
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
