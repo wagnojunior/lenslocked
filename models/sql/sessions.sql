9ad5f54f06e6021e4ffad6611ea6286089a44bec
Wagno <wagnojunior@gmail.com>
2023-01-26 21:36:20 +0900

    Deleted unused SQL files

--- models/sql/sessions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
-    token_hash TEXT UNIQUE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
