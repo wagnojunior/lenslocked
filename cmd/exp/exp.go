f1a9491b42a10fc0a7ce53410dd70b0bc6639bde
wagnojunior <wagnojunior@gmail.com>
2023-01-28 12:41:41 +0900

    Experimented with Contexts

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,144 +1,27 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-
-	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
+type ctxKey string
 
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
+const (
+	favoriteColorKey ctxKey = "favorite-color"
+)
 
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
+	ctx := context.Background()
+
+	ctx = context.WithValue(ctx, favoriteColorKey, "blue")
+	value := ctx.Value(favoriteColorKey)
+	strValue, ok := value.(int)
+	if !ok {
+		fmt.Println("Cannot asset to the desired type")
+		return
 	}
 
-	db, err := sql.Open("pgx", cfg.String())
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Connected!")
-
-	// Create a table
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			name TEXT,
-			email TEXT UNIQUE NOT NULL
-		);
-
-		CREATE TABLE IF NOT EXISTS orders (
-			id SERIAL PRIMARY KEY,
-			user_id INT NOT NULL,
-			amount INT,
-			description TEXT
-		);
-	`)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Tables created.")
-
-	// // Insert some data
-	// name := "New KKK"
-	// email := "kkaka@asdf.com"
-	// row := db.QueryRow(`
-	// 	INSERT INTO users (name, email)
-	// 	VALUES ($1, $2) RETURNING id;
-	// `, name, email)
-
-	// var id int
-	// err = row.Scan(&id)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("User created! id =", id)
-
-	// id := 1
-	// row := db.QueryRow(`
-	// 	SELECT name, email
-	// 	FROM users
-	// 	WHERE id=$1;
-	// `, id)
-	// var name, email string
-	// err = row.Scan(&name, &email)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("User information: name=%s, email=%s\n", name, email)
-
-	// userID := 2
-	// for i := 1; i <= 5; i++ {
-	// 	amount := i * 100
-	// 	desc := fmt.Sprintf("Fake order %d", i)
-	// 	_, err := db.Exec(`
-	// 		INSERT INTO orders (user_id, amount, description)
-	// 		VALUES ($1, $2, $3);
-	// 		`, userID, amount, desc)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-	// fmt.Println("Created fake orders.")
-
-	type Order struct {
-		ID          int
-		UserID      int
-		Amount      int
-		Description string
-	}
-
-	var orders []Order
-	userID := 90
-
-	rows, err := db.Query(`
-		SELECT id, amount, description
-		FROM orders
-		WHERE user_id=$1;
-		`, userID)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var order Order
-		order.UserID = userID
-		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
-		if err != nil {
-			panic(err)
-		}
-		orders = append(orders, order)
-	}
-	if rows.Err() != nil {
-		panic(rows.Err())
-	}
-
-	fmt.Println("Orders:", orders)
-
-	// check for error
+	fmt.Println(strValue)
+	// fmt.Println(strings.HasPrefix(strValue, "b"))
 }
