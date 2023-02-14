893d22e0d96584ccc83ac0619481ba23daf5e8de
wagnojunior <wagnojunior@gmail.com>
2023-02-14 20:21:39 +0900

    Added email construction

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,22 +1,44 @@
 package main
 
 import (
-	std_context "context"
 	"fmt"
+	"os"
 
-	"github.com/wagnojunior/lenslocked/context"
-	"github.com/wagnojunior/lenslocked/models"
+	"github.com/go-mail/mail/v2"
+)
+
+// These constants are retrieved from the `MailTrap` website
+const (
+	host     = "sandbox.smtp.mailtrap.io"
+	port     = 587
+	username = "540b454aea2b6e"
+	password = "0df11b87e2f0fd"
 )
 
 func main() {
-	ctx := std_context.Background()
+	// Header of the email
+	from := "test@lenslocked.com"
+	to := "wagnojunior@gmail.com"
+	subject := "This is a test email"
 
-	user := models.User{
-		Email: "test@test.com",
+	// Body of the email
+	plainText := "This is the body of the email"
+	html := `<h1> Hello there, buddy!</h1><p>This is the email</p><p>Hope you enjoy it!</p>`
+
+	// Construct the email
+	msg := mail.NewMessage()
+	msg.SetHeader("From", from)
+	msg.SetHeader("To", to)
+	msg.SetHeader("Subject", subject)
+	msg.SetBody("text/plain", plainText)
+	msg.AddAlternative("text/html", html)
+	msg.WriteTo(os.Stdout)
+
+	// Connect the the mail server and send an email
+	dialer := mail.NewDialer(host, port, username, password)
+	err := dialer.DialAndSend(msg)
+	if err != nil {
+		panic(err)
 	}
-
-	ctx = context.WithUser(ctx, &user)
-
-	retrievedUser := context.User(ctx)
-	fmt.Println(retrievedUser.Email)
+	fmt.Println("Message sent")
 }
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/derekparker/trie v0.0.0-20221221181808-1424fce0c981 // indirect
 	github.com/go-delve/delve v1.20.1 // indirect
 	github.com/go-delve/liner v1.2.3-0.20220127212407-d32d89dd2a5d // indirect
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/google/go-dap v0.7.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
@@ -44,6 +45,7 @@ require (
 	golang.org/x/arch v0.2.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
