1e645366afdc94e85239c1d4498dcfbc8b4ba691
wagnojunior <wagnojunior@gmail.com>
2023-02-16 20:51:53 +0900

    tested email service here

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/go-mail/mail/v2"
+	"github.com/wagnojunior/lenslocked/models"
 )
 
 // These constants are retrieved from the `MailTrap` website
@@ -16,29 +15,50 @@ const (
 )
 
 func main() {
-	// Header of the email
-	from := "test@lenslocked.com"
-	to := "wagnojunior@gmail.com"
-	subject := "This is a test email"
+	email := models.Email{
+		From:      "test@lenslocked.com",
+		To:        "wagnojunior@gmail.com",
+		Subject:   "This is a test email",
+		PlainText: "This is the body of the email",
+		HTML:      `<h1> Hello there, buddy!</h1><p>This is the email</p><p>Hope you enjoy it!</p>`,
+	}
 
-	// Body of the email
-	plainText := "This is the body of the email"
-	html := `<h1> Hello there, buddy!</h1><p>This is the email</p><p>Hope you enjoy it!</p>`
+	es := models.NewEmailService(models.SMTPConfig{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+	})
 
-	// Construct the email
-	msg := mail.NewMessage()
-	msg.SetHeader("From", from)
-	msg.SetHeader("To", to)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/plain", plainText)
-	msg.AddAlternative("text/html", html)
-	msg.WriteTo(os.Stdout)
-
-	// Connect the the mail server and send an email
-	dialer := mail.NewDialer(host, port, username, password)
-	err := dialer.DialAndSend(msg)
+	err := es.Send(email)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Message sent")
+	fmt.Println("email sent")
+
+	// // Header of the email
+	// from := "test@lenslocked.com"
+	// to := "wagnojunior@gmail.com"
+	// subject := "This is a test email"
+
+	// // Body of the email
+	// plainText := "This is the body of the email"
+	// html := `<h1> Hello there, buddy!</h1><p>This is the email</p><p>Hope you enjoy it!</p>`
+
+	// // Construct the email
+	// msg := mail.NewMessage()
+	// msg.SetHeader("From", from)
+	// msg.SetHeader("To", to)
+	// msg.SetHeader("Subject", subject)
+	// msg.SetBody("text/plain", plainText)
+	// msg.AddAlternative("text/html", html)
+	// msg.WriteTo(os.Stdout)
+
+	// // Connect the the mail server and send an email
+	// dialer := mail.NewDialer(host, port, username, password)
+	// err := dialer.DialAndSend(msg)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Message sent")
 }
