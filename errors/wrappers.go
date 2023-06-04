4e33305737f0373de47d4ff11040524c2439b00f
wagnojunior <wagnojunior@gmail.com>
2023-06-04 15:53:29 +0900

    Created a wrapper to wrap the custom error
    - added As and Is functions from the std lib

---
+++ errors/wrappers.go
@@ -0,0 +1,10 @@
+package errors
+
+import "errors"
+
+// These variables are used to give access to existing functions in the standard
+// library `errors` package
+var (
+	As = errors.As
+	Is = errors.Is
+)
