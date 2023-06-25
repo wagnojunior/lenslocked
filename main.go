bec9116cf0acbb7b41c8b1d6e6354d7f2d92a76a
wagnojunior <wagnojunior@gmail.com>
2023-06-25 18:33:55 +0900

    Added endpoint to publish/unpublish a gallery

--- main.go
+++ main.go
@@ -186,6 +186,8 @@ func main() {
 			r.Get("/{id}/edit", galleriesC.Edit)
 			r.Post("/{id}", galleriesC.Update)
 			r.Post("/{id}/delete", galleriesC.Delete)
+			r.Post("/{id}/publish", galleriesC.Publish)
+			r.Post("/{id}/unpublish", galleriesC.Unpublish)
 		})
 
 	})
