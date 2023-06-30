c8d51e882210775c960b95fe88ee79df431d8f8b
wagnojunior <wagnojunior@gmail.com>
2023-06-30 14:23:21 +0900

    Added router to show images

--- main.go
+++ main.go
@@ -178,6 +178,7 @@ func main() {
 	r.Route("/galleries", func(r chi.Router) {
 		// r.Group groups all paths to the same middleware
 		r.Get("/{id}", galleriesC.Show)
+		r.Get("/{id}/images/{filename}", galleriesC.Image)
 		r.Group(func(r chi.Router) {
 			r.Use(umw.RequireUser)
 			r.Get("/new", galleriesC.New)
